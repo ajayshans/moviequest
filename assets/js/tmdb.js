eca1895de7cc33fcb63df3f9033831eb8265563d
Ajay S <ajaycshan@gmail.com>
2023-09-26 18:20:09 +1000

    Add base JS files

---
+++ assets/js/tmdb.js
@@ -0,0 +1,1 @@
+// This consists of all Javascript related to The Movie Database Functions while working so testing can be conducted separately. This will be combined into a single JS towards the end of the project
