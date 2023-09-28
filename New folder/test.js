514b58edb5b8b020b056c3b3f69eed5f4f95aa31
Julian Abouzeid <Julian@wheresnick.com.au>
2023-09-28 20:41:55 +1000

    added drop down search functionality and the decade function

--- New folder/test.js
+++ New folder/test.js
@@ -1,8 +1,19 @@
-function searchAPI(event, genreId) {
-    event.preventDefault();
+document.getElementById('searchButton').addEventListener('click', function() { // select dropdown //
+    var selectedGenre = document.getElementById('dropdown-genre').value;
+    var selectedDecade = document.getElementById('dropdown-decade').value;
 
-    // Step 1: Fetch the first page to determine total pages
+    searchAPI(selectedGenre, selectedDecade);
+});
+
+function searchAPI(genreId, decade) {  // searching first for genre to take into account any date option //
     var apiUrl = 'https://api.themoviedb.org/3/discover/movie?api_key=0a5329b198d874cdfd05a37c220a128b&with_genres=' + genreId;
+
+    // then added the date option here //
+    if (decade !== "Any") {
+    var startDate = decade + '-01-01';
+    var endDate = (parseInt(decade) + 9).toString() + '-12-31';
+    apiUrl += '&primary_release_date.gte=' + startDate + '&primary_release_date.lte=' + endDate;
+    }
 
     fetch(apiUrl)
     .then(response => response.json())
@@ -10,12 +21,13 @@ function searchAPI(event, genreId) {
         var maxPages = Math.min(data.total_pages, 100);
         var randomPage = Math.floor(Math.random() * maxPages) + 1;
       
-        return fetch(apiUrl + '&page=' + randomPage); // Step 2: Fetch the randomly selected page
+        return fetch(apiUrl + '&page=' + randomPage); // Fetch the randomly selected page
     })
+    // post search functionality below //
     .then(response => response.json())
     .then(data => {
         if (data.results && data.results.length) {
-            // Step 4: Randomly select a movie from the results of the randomly selected page
+            // Randomly select a movie from the results of the randomly selected page
             var randomIndex = Math.floor(Math.random() * data.results.length);
             var randomMovie = data.results[randomIndex];
 
