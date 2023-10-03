8d1d7ec4eb6e1254df3f92238e28d4e976be7a2f
Ajay S <ajaycshan@gmail.com>
2023-10-03 19:30:15 +1100

    Test

--- assets/js/youtube.js
+++ assets/js/youtube.js
@@ -1,5 +1,7 @@
 // // // This consists of all Javascript related to Youtube Functions while working so testing can be conducted separately. This will be combined into a single JS towards the end of the project
 
+console.log('Test')
+
 function getVideoURL(sampleTitle) {
     var ytApiKey = "AIzaSyB8PzB4thIJ1yyzTuJhkEhy3FlEEIZVsJg"
     searchInput = sampleTitle.toLowerCase().replace(" ", "+") + "+movie+trailer"
@@ -9,5 +11,10 @@ function getVideoURL(sampleTitle) {
         return response.json();})
 }
 
-var sampleTitle = "Shawshank Redemption" // Note: we will use latestMovieTitle here when combining the 2 JS files
-console.log(getVideoURL(sampleTitle))
+var Test = "Shawshank Redemption" // Note: we will use latestMovieTitle here when combining the 2 JS files
+ytApiKeyt = "AIzaSyB8PzB4thIJ1yyzTuJhkEhy3FlEEIZVsJg"
+searchInputt = Test.toLowerCase().replace(" ", "+") + "+movie+trailer"
+searchResultt = "https://www.googleapis.com/youtube/v3/search?key=" + ytApiKeyt + "&part=snippet&type=video&q=" + searchInputt;
+console.log(searchResultt)
+console.log(getVideoURL(Test))
+console.log('End')
